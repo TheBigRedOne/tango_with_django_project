d110b114249009c2a673d11ef379c629931d5d96
Yuting Wan <bb71louisiana@gmail.com>
2020-02-18 09:23:32 +0000

    添加项目文件。

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\USNORTHCOM\source\repos\tango_with_django_project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
